3e6d4d10465036f2c188cb869fb13828e04e801b
usausa <machi.pon@gmail.com>
2022-02-18 20:12:34 +0900

    Add project baseline

---
+++ Template.Server/GlobalUsing.cs
@@ -0,0 +1,37 @@
+// ReSharper disable RedundantUsingDirective.Global
+#pragma warning disable
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.ComponentModel.DataAnnotations;
+global using System.Data.Common;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Globalization;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+//global using AutoMapper;
+
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+
+global using Microsoft.Extensions.Logging;
+
+//global using Smart;
+//global using Smart.Collections.Generic;
+//global using Smart.Text;
+
+//global using Template;
+//global using Template.Domain;
+//global using Template.Models.Connector;
+//global using Template.Models.Entity;
+//global using Template.Models.Paging;
+//global using Template.Models.View;
+//global using Template.Services;
+//global using Template.Web.Authentication;
+//global using Template.Web.Infrastructure.Filters;
+//global using Template.Web.Infrastructure.Mvc;
+//global using Template.Web.Infrastructure.Token;
+//global using Template.Web.Settings;
---
+++ Template.Server/Pages/Error.cshtml.cs
@@ -0,0 +1,20 @@
+namespace Template.Server.Pages;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using System.Diagnostics;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class ErrorModel : PageModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !String.IsNullOrEmpty(RequestId);
+
+    public void OnGet()
+    {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+    }
+}
