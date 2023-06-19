bb2f6e15af51e93cc766ce4f6b446b7b5f3739e8
usausa <machi.pon@gmail.com>
2023-06-19 10:44:37 +0900

    Update startup

--- Template.Server/Program.cs
+++ Template.Server/Program.cs
@@ -39,8 +39,6 @@ Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 //--------------------------------------------------------------------------------
 // Configure builder
 //--------------------------------------------------------------------------------
-Directory.SetCurrentDirectory(AppContext.BaseDirectory);
-
 // Configure builder
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
@@ -151,7 +149,7 @@ var connectionStringBuilder = new SqliteConnectionStringBuilder
 var connectionString = connectionStringBuilder.ConnectionString;
 builder.Services.AddSingleton<IDbProvider>(new DelegateDbProvider(() => new SqliteConnection(connectionString)));
 builder.Services.AddSingleton<IDialect>(new DelegateDialect(
-    static ex => (ex as SqliteException)?.SqliteErrorCode == 1555,
+    static ex => ex is SqliteException { SqliteErrorCode: 1555 },
     static x => Regex.Replace(x, @"[%_]", "[$0]")));
 builder.Services.AddDataAccessor();
 
