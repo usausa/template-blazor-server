c47a1481f18ba008fcff389965d12a8de8a28893
usausa <machi.pon@gmail.com>
2025-07-24 09:36:05 +0900

    Update setting

--- Template.Server.Tests/Pages/LoginPageTest.cs
+++ Template.Server.Tests/Pages/LoginPageTest.cs
@@ -1,10 +1,14 @@
 namespace Template.Server.Pages;
 
+using Template.Components.Storage;
+
 public sealed class LoginPageTest
 {
     [Fact]
     public void Test()
     {
         // TODO
+        var service = new FileStorage(new FileStorageOptions { Root = "." });
+        Assert.NotNull(service);
     }
 }
