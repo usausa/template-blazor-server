7e2f66a6b03cab5d8c2eb1495e0fc32b43e3ac83
usausa <machi.pon@gmail.com>
2026-09-12 14:04:21 +0900

    Update

--- src/Template.BlazorServer.Host/Application/ApplicationExtensions.cs
+++ src/Template.BlazorServer.Host/Application/ApplicationExtensions.cs
@@ -492,7 +492,7 @@ public static class ApplicationExtensions
         builder.Services.AddSingleton(new DefaultPasswordProviderOptions());
         builder.Services.AddSingleton<IPasswordProvider, DefaultPasswordProvider>();
 
-        // Service & Usecase
+        // Service
         builder.Services.AddCoreServices();
 
         // Notification
